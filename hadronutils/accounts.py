6442fbbd74740997b3ae2c7ce3a952f319b16e34
StuartFarmer <email@stuartfarmer.me>
2017-08-06 13:45:16 -0400

    deploy and classmethods added to contracts

--- hadronutils/accounts.py
+++ hadronutils/accounts.py
@@ -6,15 +6,15 @@ import json
 import os
 from web3 import Web3
 from web3.personal import Personal
-from web3 import Eth
+from web3.eth import Eth
 import utils
 
 class Account:
 	@classmethod
 	def new(self, name=None, password=None, chain=None):
-		assert name != None and password != None and chain != None
+		assert name != None and password != None and chain != None, 'Missing information needed to create an account'
 		# check if name already exists in db. if not, continue
-		assert Chain().database.select_account(name) is None
+		assert Chain().database.select_account(name) is None, 'Account with the same name already exists in the database'
 		address = utils.create_account(password)
 		
 		a = Account()
@@ -23,6 +23,9 @@ class Account:
 
 		Chain().database.insert_account(a)
 
+	@classmethod
+	def from_db(self, name=None, address=None):
+		assert name != None or address != None, 'Supply either a name or an address to query the DB with'
 	#TODO
 	#fancy shit making interacting with the blockchain easy (get balance, transact, etc)
 	def balance(self):
--- hadronutils/contracts.py
+++ hadronutils/contracts.py
@@ -10,6 +10,8 @@ from genesis import Chain
 
 import web3
 from web3.eth import Eth
+from solc import compile_source, compile_standard
+
 from jinja2 import Environment
 from jinja2.nodes import Name
 
@@ -19,12 +21,6 @@ import utils
 DEFAULT_CONTRACT_DIRECTORY = './contracts'
 
 class Contract():
-	chain = None
-	name = None
-	is_deployed = None
-	address = None
-	instance = None
-
 	def __init__(self):
 		self.chain = Chain()
 
@@ -32,26 +28,75 @@ class Contract():
 		return 'Contract {}, {}'.format(self.address if self.address else 'None', self.name if self.name else 'None')
 
 	def from_db(self, name=None, address=None):
-		assert self.chain, "No chain provided."
+		assert self.chain, 'No chain provided.'
 		self = Contract.from_db(name=name, address=address)
 
+	def from_chain(self):
+		pass
+
 	def load_sol_file(self, file=None):
-		assert self.chain, "No chain provided."
-		assert file, "No file provided"
+		assert self.chain, 'No chain provided.'
+		assert file, 'No file provided'
 
 		self.sol = file.read()
 
-		pass
-
 	def load_tsol_file(self, file=None, payload=None):
-		assert self.chain, "No chain provided."
-		assert file and payload, "No file or payload provided"
+		assert self.chain, 'No chain provided.'
+		assert file and payload, 'No file or payload provided.'
 
 		payload['sol'] = file.read()
 
 		self.sol = Accounts.generate_new_contract(payload=payload)
 
-		pass
+	def deploy(self):
+		assert not self.is_deployed, 'This contract already exists on the chain.'
+		assert self.sol, 'No solidity code loaded into this object'
+
+		#compile sol
+		input_json = {
+			'language': 'Solidity',
+			'sources': {
+				self.name: {
+					'content': self.sol
+				}
+			},
+			'settings': {
+				'outputSelection': {
+					'*': {
+						'*': [ 'metadata', 'evm.bytecode', 'abi', 'evm.bytecode.opcodes', 'evm.gasEstimates', 'evm.methodIdentifiers' ]
+					}
+				}
+			}
+		}
+
+		output_json = compile_standard(input_json)
+
+		compiled_name = list(output_json['contracts'][self.name].keys())[0]
+
+		#break output into bits to store into the db for simple access
+		self.abi = output_json['contracts'][self.name][compiled_name]['abi']
+		self.metadata = output_json['contracts'][self.name][compiled_name]['metadata']
+		self.bytecode = output_json['contracts'][self.name][compiled_name]['evm']['deployedBytecode']['object']
+		self.gas_estimate = output_json['contracts'][self.name][compiled_name]['evm']['gasEstimates']
+		self.method_identifiers = output_json['contracts'][self.name][compiled_name]['evm']['methodIdentifiers']
+
+		#pickle the blobs and add them to the db
+
+		#deploy to the blockchain
+
+		#update the deployed and address to the db
+
+	@classmethod
+	def new(self, name):
+		assert name != None, 'A name identifier must be provided to create a new contract instance.'
+		assert name_is_unique(name), 'A unique identifier must be provided.'
+		c = Contract()
+		c.name = name
+		return c
+
+	@classmethod
+	def name_is_unique(self, name):
+		return True if Contract().from_db(name) != None else False
 
 	@classmethod
 	def get_template_variables(self, fo):
@@ -66,7 +111,7 @@ class Contract():
 			try:
 				c.instance = Eth.contract(name=c.name, address=c.address)
 			except:
-				pass
+				c = None
 		return c
 
 	@classmethod
