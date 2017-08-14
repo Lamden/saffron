4ae06e6657fab7717f481af5c5062c273f9c4813
jmunsch <james.a.munsch@gmail.com>
2017-08-14 12:45:24 -0700

    cli tools for cli users

--- hadronutils/settings.py
+++ hadronutils/settings.py
@@ -1,9 +1,16 @@
-import os
-WORKING_DIR = '../test_project'
-cdir = os.path.dirname(__file__)
-WORKING_DIR = os.path.abspath(os.path.join(cdir, WORKING_DIR))
-DB_FILE = os.path.abspath(os.path.join(WORKING_DIR, 'directory.db'))
+import os, configparser
+from os.path import join
+run_location, filename = os.path.split(os.path.abspath(__file__))
+config = configparser.ConfigParser()
+
+config.read(os.path.join(run_location, 'config/default.conf'))
+hadron_home = os.environ.get('HADRON_HOME', None) if os.environ.get('HADRON_HOME', None) else os.path.expanduser(config.defaults()['hadron_home']) # ~/.hadron
+hadron_folder_path = os.environ.get('HADRON_FOLDER_PATH', None) if os.environ.get('HADRON_FOLDER_PATH', None) else join(hadron_home, config.defaults()['hadron_folder_path']) # ~/.hadron/hadron-default
+hadron_db_file = os.environ.get('HADRON_DB_FILE', None) if os.environ.get('HADRON_DB_FILE', None) else join(hadron_folder_path, config.defaults()['hadron_db_file']) # ~/
+
+
+
 try:
-    os.mkdir(WORKING_DIR)
+    os.makedirs(hadron_folder_path)
 except OSError as e:
     pass
